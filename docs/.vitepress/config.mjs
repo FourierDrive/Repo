ac887f8e0b6eb3f52c4b3ce42c7ab83e6291894d
FourierDrive <tt14283065@gmail.com>
2026-09-07 16:38:18 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -17,8 +17,11 @@ export default {
               items: [
                 { text: 'Terminology', link: '/storage/nandflash/terminology' },
                 { text: 'Controller Interface', link: '/storage/nandflash/controller_interface' },
-                { text: 'Operations', link: '/storage/nandflash/operations' },
-                { text: 'Management', link: '/storage/nandflash/management' }
+                { text: 'Read Operations', link: '/storage/nandflash/read_operations' },
+                { text: 'Program Operations', link: '/storage/nandflash/program_operations' },
+                { text: 'Erase Operations', link: '/storage/nandflash/erase_operations' },
+                { text: 'Suspend and Resume', link: '/storage/nandflash/suspend_and_resume' },
+                { text: 'Bad Block Management', link: '/storage/nandflash/bad_block_management' }
               ]
             },
             {
